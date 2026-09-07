c21bcd3a0c12732479862ab0343b8f131d8682b1
malysh-vale <sashiilopez7@gmail.com>
2026-09-06 21:30:47 -0300

    Add state management for layer properties

--- DibujoApp/DibujoApp/app/src/main/java/com/malysh/dibujoapp/LayerModel.kt
+++ DibujoApp/DibujoApp/app/src/main/java/com/malysh/dibujoapp/LayerModel.kt
@@ -1,7 +1,9 @@
 package com.malysh.dibujoapp
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 
 class LayerModel(
