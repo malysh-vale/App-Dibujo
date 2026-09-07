385bc57cad86c24ba8884ce0ff6b9f6fbd867304
malysh-vale <sashiilopez7@gmail.com>
2026-09-06 21:24:33 -0300

    Add LayerModel class for drawing layers

---
+++ DibujoApp/DibujoApp/app/src/main/java/com/malysh/dibujoapp/LayerModel.kt
@@ -0,0 +1,14 @@
+package com.malysh.dibujoapp
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+
+class LayerModel(
+    val id: String,
+    name: String = "Capa"
+) {
+    var name by mutableStateOf(name)
+    var visible by mutableStateOf(true)
+    val strokes: SnapshotStateList<DrawnStroke> = mutableStateListOf()
+}
